efbe542b5e091eb747cb243f9060bc3ece49bb3a
soifuturegit <soifuture.git@gmail.com>
2025-10-20 15:04:19 +0600

    Updated Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -177,7 +177,7 @@ export default function Projects() {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
         {filteredProjects.map((project, index) => (
           <div
             key={index}
